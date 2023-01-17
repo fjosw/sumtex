c977d9a7f04bdfaba38f9808a7f680e6f40c33a0
Fabian Joswig <fabian.joswig@ed.ac.uk>
2023-01-17 11:23:24 +0000

    feat: reduced the number of calls to the openai API.

--- sumtex/__main__.py
+++ sumtex/__main__.py
@@ -6,17 +6,11 @@ if __name__ == '__main__':
     sections = latex.process_latex_file(sys.argv[1])
 
     my_prompts = [{"context": "",
-                   "prompt": "Give the main topic of the following text, the three most important points and the audience",
-                   "temperature": 0.0},
-                  {"context": "Suggested title:",
-                   "prompt": "What title would you give to the following text?",
-                   "temperature": 0.0},
-                  {"context": "Catchy title:",
-                   "prompt": "Give a sensationalist title for the following text",
-                   "temperature": 0.8},
-                  {"context": "Understandability rating out of 10:",
-                   "prompt": "How understandable is the following text for a domain expert on a a scale from 0 to 10? Just return a number.",
-                   "temperature": 0.0}]
+                   "prompt": "Give the main topic of the following text, the three most important points and the audience and rate the understandability on a scale from 0 to 10.",
+                   "temperature": 0.1},
+                  {"context": "Title suggestions:",
+                   "prompt": "Generate an objective and a catchy title for the following text.",
+                   "temperature": 0.8}]  # Add additional prompts here.
 
     for sec_name, sec_text in sections.items():
         sec_name_string = f"Section: {sec_name}"
